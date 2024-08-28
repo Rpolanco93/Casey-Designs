ea8032f5ab3fe732da47946846b005f210e4f9e8
Ruben Polanco <andr3z.93@gmail.com>
2024-08-28 15:11:37 -0700

    updated docker file due to error in production

--- migrations/tools/scripts/flyway/helper.sh
+++ migrations/tools/scripts/flyway/helper.sh
@@ -5,8 +5,10 @@ base_directory() {
   cd -P "$( dirname "$( readlink -f "${BASH_SOURCE[0]}" )" )" && pwd
 }
 
-# Source env
-source "$(base_directory)/../../../../.env"
+# Source env if DATABASE_URL isn't present
+if [ -z "${DATABASE_URL}" ]; then
+    source "$(base_directory)/../../../../.env"
+fi
 
 # Flyway
 _FLYWAY="$(base_directory)/../../bin/flyway/flyway"

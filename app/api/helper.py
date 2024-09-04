b91496adb8f8a0aef65551e7c8edf510209e3af6
Rpolanco93 <131895581+Rpolanco93@users.noreply.github.com>
2024-09-03 18:33:47 -0700

    setup helper file for aws (#6)

--- app/api/helper.py
+++ app/api/helper.py
@@ -1,4 +1,60 @@
-# function to convert db results to dict
+import uuid, os, boto3, botocore
+
+'''AWS connection setup'''
+
+s3 = boto3.client(
+   "s3",
+   aws_access_key_id=os.environ.get("S3_KEY"),
+   aws_secret_access_key=os.environ.get("S3_SECRET")
+)
+
+'''Helper function to generate unique file names for s3'''
+
+ALLOWED_EXTENSIONS = {"pdf", "png", "jpg", "jpeg", "gif"}
+
+def get_unique_filename(filename):
+    ext = filename.rsplit(".", 1)[1].lower()
+    unique_filename = uuid.uuid4().hex
+    return f"{unique_filename}.{ext}"
+
+'''Upload File to s2 helper'''
+
+BUCKET_NAME = os.environ.get("S3_BUCKET")
+S3_LOCATION = f"http://{BUCKET_NAME}.s3.amazonaws.com/"
+
+def upload_file_to_s3(file, acl="public-read"):
+    try:
+        s3.upload_fileobj(
+            file,
+            BUCKET_NAME,
+            file.filename,
+            ExtraArgs={
+                "ACL": acl,
+                "ContentType": file.content_type
+            }
+        )
+    except Exception as e:
+        # in case the your s3 upload fails
+        return {"errors": str(e)}
+
+    return {"url": f"{S3_LOCATION}{file.filename}"}
+
+'''Delete file from s3 helper'''
+def remove_file_from_s3(image_url):
+    # AWS needs the image file name, not the URL,
+    # so you split that out of the URL
+    key = image_url.rsplit("/", 1)[1]
+    try:
+        s3.delete_object(
+        Bucket=BUCKET_NAME,
+        Key=key
+        )
+    except Exception as e:
+        return { "errors": str(e) }
+    return True
+
+'''Function to convert db results to dict'''
+
 def make_dict(product):
     # fetch first image
     preview_image = product.product_images[0].url if len(product.product_images) else None

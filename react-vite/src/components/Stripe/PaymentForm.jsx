5aefc46d26c531b270cfc105a51e1b66009a9112
Ruben Polanco <andr3z.93@gmail.com>
2024-10-02 20:23:26 -0700

    created the checkout form component

--- react-vite/src/components/Stripe/PaymentForm.jsx
+++ react-vite/src/components/Stripe/PaymentForm.jsx
@@ -1,8 +1,16 @@
 import {useEffect, useState} from "react";
+import { PaymentElement } from "@stripe/react-stripe-js";
+import { useStripe, useElements } from "@stripe/react-stripe-js";
 
 function PaymentForm() {
+    const stripe = useStripe();
+    const elements = useElements();
+
     const [stripePromise, setStripePromise] = useState(null);
     const [clientSecret, setClientSecret] = useState("");
+
+    const [message, setMessage] = useState("");
+    const [isProcessing, setIsProcessing] = useState(false);
 
     useEffect(() => {
         fetch("/api/config").then(
@@ -20,13 +28,49 @@ function PaymentForm() {
         }).then(async (response) => {
             const { clientSecret } = await response.json();
 
-            console.log(clientSecret)
+            setClientSecret(clientSecret);
         })
     }, []);
 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if (!stripe || !elements) {
+            // Stripe.js has not yet loaded.
+            // Make sure to disable form submission until Stripe.js has loaded.
+            return;
+        }
+
+        setIsProcessing(true);
+
+        const { error } = await stripe.confirmPayment({
+            elements,
+            confirmParams: {
+                return_url: `${window.location.origin}/payment-completed`,
+            }
+        });
+
+        if (error.type === "card_error" || error.type === "validation_error") {
+            setMessage(error.message);
+        } else {
+            setMessage("An unexpected error has occurred");
+        }
+
+        setIsProcessing(false);
+    }
+
     return (
         <div className="payment-form">
-            <h2>Payment Form</h2>
+            <form id="payment-form" onSubmit={handleSubmit}>
+                <PaymentElement id="payment-element" />
+                <button disabled={isProcessing || !stripe || !elements} id="submit">
+                    <span id="payment-button-text">
+                        {isProcessing ? "Processing ... " : "Pay now"}
+                    </span>
+                </button>
+                {/* Show any error or success messages */}
+                {message && <div id="payment-message">{message}</div>}
+            </form>
         </div>
     )
 }
